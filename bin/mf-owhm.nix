9097b6ba7fea6268c111df28e74fc3d9016652a6
Scott Boyce <seboyce@usgs.gov>
2021-04-23 08:45:45 -0700

    2.0.2 preliminary release
    Boyce, S.E., 2021, MODFLOW One-Water Hydrologic Flow Model (MF-OWHM)
    Conjunctive Use and Integrated Hydrologic Flow Modeling Software,
    version 2.0.2: U.S. Geological Survey Software Release,
    https://doi.org/10.5066/P9P8I8GS
    
    Binaries compiled:
      bin/mf-owhm-gmg.exe
      bin/mf-owhm.exe
      bin/mf-owhm.nix

--- bin/mf-owhm.nix
+++ bin/mf-owhm.nix
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0afcef4c879d93991eb6fb022ad05c3a9e19c82fd05d84c44f15e17a7ee9ef87
-size 17415776
+oid sha256:ca27b0c4e47e63b0b8992064f1acd69078507b233fca75f65d932f16b570d239
+size 18269360
