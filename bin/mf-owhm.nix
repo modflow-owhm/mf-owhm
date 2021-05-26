ce2ed1cb7c63aa2ddd097c7dcda93388a2887b4d
Scott Boyce <seboyce@usgs.gov>
2021-05-25 23:24:25 -0700

    2.1.0 production release
    IPDS: IP-129524
    
    Boyce, S.E. and Ferguson, I.M., 2021, MODFLOW One-Water Hydrologic Flow Model
    (MF-OWHM) Conjunctive Use and Integrated Hydrologic Flow Modeling Software
    with Surface Water Operations, version 2.1.0: U.S. Geological Survey Software Release,
    https://doi.org/10.5066/2FP9C6F6C5
    
    Boyce, S.E., 2021, MODFLOW One-Water Hydrologic Flow Model (MF-OWHM)
    Conjunctive Use and Integrated Hydrologic Flow Modeling Software,
    version 2.1.0: U.S. Geological Survey Software Release,
    https://doi.org/10.5066/P9P8I8GS
    
    Ferguson, I.M.., Llewellyn, D., Hanson, R.T., and Boyce S.E., 2016,
    User guide to the surface water operations process—An integrated approach to
    simulating large-scale surface water management in MODFLOW-based hydrologic models:
    Denver, Colo., Bureau of Reclamation Technical Memorandum no. 86-68210–2016-02, 96 p.
    
    Binaries compiled:
      bin/mf-owhm-gmg.exe
      bin/mf-owhm.exe
      bin/mf-owhm.nix

--- bin/mf-owhm.nix
+++ bin/mf-owhm.nix
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8dbce619b23709c3e36d26d91f94c9df6a2f863d6ef538357a49b097845a7292
-size 18296632
+oid sha256:6db56c910c3c0224146e7aa8941957291dadecd1ec9f8ee8873bd621f01ba6d4
+size 21365048
