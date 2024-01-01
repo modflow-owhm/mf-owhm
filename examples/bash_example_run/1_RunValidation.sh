8ae48f48f0fde9ffb9838421ebfe452abc886fa8
Scott Boyce <seboyce@usgs.gov>
2023-12-31 23:59:59 -0800

    refactor: 1_RunValidation.sh optimize call order for fast argument
    When 1_RunValidation.sh has the fast argument:
    
    bash  1_RunValidation.sh  fast
    
    then the examples are called in parallel groups.
    The examples were grouped such they runs with similar
    runtime are grouped together.
    
    This improves the overall runtime

--- examples/bash_example_run/1_RunValidation.sh
+++ examples/bash_example_run/1_RunValidation.sh
@@ -7,7 +7,7 @@
 #    all      -> include running extremely long test cases
 #    check    -> skip running examples and only CHECK the results in the output directories
 #    debug    -> runs mf-owhm_debug program instead of mf-owhm
-#    fast     -> ZYZ run test examples as background processes (more than one at once)
+#    fast     -> run test examples as background processes (more than one at once)
 #    nopause  -> disables requiring the user to hit pause at the end of the script (auto ends)
 #    save     -> do not delete intermediate testing program mf_owhm_validate_example_results
 #    reuse    -> if mf_owhm_validate_example_results already exists, reuse it instead of recompiling
@@ -65,10 +65,15 @@ done
 
 if [ "${runExamples}" = "T" ]
 then
+   source ./0_SelectProgramUsedByBASH.sh $debug
+   cd "$SHELLDIR"
+
    echo
    echo "  Cleaning out the old example files"
    echo
    bash ../../.util/clean/cleanExampleOutput.sh
+   echo
+   echo "  Test binary: ${OWHM}"
    echo
    curTim=`date +"%T"`
    echo "  Starting to run tests at ${curTim}"
@@ -105,20 +110,20 @@ then
       trap "exit" INT TERM ERR
       trap "kill 0" EXIT
       
-      bash 2_RunAllTests_MF_2005.sh     "nopause"  $debug  > out1_MF_2005.txt     & echo "    MF_2005      test 1 of 8 -> Started"
       bash 2_RunAllTests_MF_2005_NWT.sh "nopause"  $debug  > out2_MF_2005_NWT.txt & echo "    MF_2005_NWT  test 2 of 8 -> Started"
       bash 2_RunAllTests_MF_CFP.sh      "nopause"  $debug  > out3_MF_CFP.txt      & echo "    MF_CFP       test 3 of 8 -> Started"
-      wait
-      echo "                                Tests 1, 2, 3 Finished"
-      bash 2_RunAllTests_MF_NWT.sh      "nopause"  $debug  > out4_MF_NWT.txt      & echo "    MF_NWT       test 4 of 8 -> Started"
       bash 2_RunAllTests_MF_OWHM.sh     "nopause"  $debug  > out5_MF_OWHM.txt     & echo "    MF_OWHM      test 5 of 8 -> Started"
       bash 2_RunAllTests_MF_RIP.sh      "nopause"  $debug  > out6_MF_RIP.txt      & echo "    MF_RIP       test 6 of 8 -> Started"
       wait
-      echo "                                Tests 4, 5, 6 Finished"
+      echo "                                Tests 2, 3, 5, 6 Finished"
+      bash 2_RunAllTests_MF_NWT.sh      "nopause"  $debug  > out4_MF_NWT.txt      & echo "    MF_NWT       test 4 of 8 -> Started"
       bash 2_RunAllTests_MF_SWI.sh      "nopause"  $debug  > out7_MF_SWI.txt      & echo "    MF_SWI       test 7 of 8 -> Started"
+      wait
+      echo "                                Tests 4, 7 Finished"
+      bash 2_RunAllTests_MF_2005.sh     "nopause"  $debug  > out1_MF_2005.txt     & echo "    MF_2005      test 1 of 8 -> Started"
       bash 2_RunAllTests_MF_SWR.sh      "nopause"  $debug  > out8_MF_SWR.txt      & echo "    MF_SWR       test 8 of 8 -> Started"
       wait
-      echo "                                Tests 7, 8 Finished"
+      echo "                                Tests 1, 8 Finished"
       echo
    fi
 
